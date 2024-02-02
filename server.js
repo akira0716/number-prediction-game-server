968fb10f00fb2588635c335cde01f3b3a7091e2f
akira0716 <akira.inaji.0716@gmail.com>
2024-02-02 22:54:39 +0900

    修正

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
-module.exports.io = new Server(server);
+module.exports.io = new Server(server, {
+  cors: {
+    origin: process.env.REACT_APP_CLIENT,
+  },
+});
 
 server.listen(3000, () => {
   console.log(`server running`);
